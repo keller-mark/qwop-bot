042d5bbff099cf5d4445951be28a84580b391510
ssaltwick <sam@saltwick.com>
2018-04-07 16:42:35 -0400

    sel.py updates

--- sel.py
+++ sel.py
@@ -5,13 +5,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 import pyautogui
 import os
 import time
-
+RIGHT = 'd'
+UP = 'w'
 SP = "clblHP"
 GO = "wbc-btn-go"
 ch_team = "cTXYfN"
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(chrome_options=chrome_options)
+#chrome_options = Options()
+#chrome_options.add_argument("--no-sandbox")
+#driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Firefox()
 path = os.path.abspath(os.path.join("slime", "index.html"))
 driver.get("file://" + path)
 driver.find_element_by_class_name(SP).click()
@@ -21,9 +23,8 @@ for i in range(0,13):
 driver.find_element_by_class_name(GO).click()
 driver.find_element_by_class_name(GO).click()
 time.sleep(0.01)
-pyautogui.keyDown('d')
+ActionChains(driver).key_down(RIGHT).perform()
 time.sleep(1)
-pyautogui.keyUp('d')
+ActionChains(driver).key_up(RIGHT).perform()
 time.sleep(1)
-
 driver.quit()
