247e9e94a1c9aa5a2b6f4cf90ab1796d80c28bf5
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-04-07 15:47:24 -0400

    Restructure

--- sel.py
+++ sel.py
@@ -11,6 +11,6 @@ chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 #driver.get("file:///bot/slime/index.html")
-path = os.path.abspath("index.html")
+path = os.path.abspath(os.path.join("slime", "index.html"))
 driver.get("file://" + path)
-driver.find_element_by_class_name("clblHP").click();
+driver.find_element_by_class_name("clblHP").click()
