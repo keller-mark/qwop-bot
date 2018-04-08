a13be518f9d43006f1c67903037e605fcba3d30b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-04-08 12:05:00 -0400

    clean up

--- sel.py
+++ sel.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.firefox.options import Options
-import pyautogui
 import os
 import time
 import json
